eb52a3858bf8d365bba78d7d4d6359565f160ce2
impulse <ecklf@icloud.com>
2019-10-31 17:09:38 +0100

    style(tw): add tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,40 @@
 module.exports = {
   theme: {
+    fontFamily: {
+      display: [
+        "Inter",
+        "-apple-system",
+        "BlinkMacSystemFont",
+        '"Segoe UI"',
+        "Roboto",
+        '"Helvetica Neue"',
+        "Arial",
+        '"Noto Sans"',
+        "sans-serif",
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+        '"Noto Color Emoji"'
+      ]
+    },
+    customForms: theme => ({
+      default: {
+        input: {
+          backgroundColor: theme("colors.gray.900"),
+          "&::placeholder": {
+            color: theme("colors.gray.500"),
+            opacity: "1"
+          },
+          "&:focus": {
+            outline: "none",
+            boxShadow: theme("boxShadow.none"),
+            borderColor: theme("colors.orange.500")
+          }
+        }
+      }
+    }),
     extend: {}
   },
   variants: {},
-  plugins: []
+  plugins: [require("@tailwindcss/custom-forms")]
 };
