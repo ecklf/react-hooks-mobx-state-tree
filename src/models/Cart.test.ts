a682b3a2ea0827a281e117c9bbc81b854037c937
impulse <ecklf@icloud.com>
2019-10-31 17:08:26 +0100

    test(jest): add test files and snapshots

--- src/models/Cart.test.ts
+++ src/models/Cart.test.ts
@@ -1,22 +1,24 @@
 import { CartItem, Cart } from "./Cart";
-import { getSnapshot } from "mobx-state-tree";
+import { getSnapshot, onSnapshot, onPatch } from "mobx-state-tree";
+import { reaction } from "mobx";
 
-it("can create an instance of a model", () => {
+it("should create a model instance", () => {
   const item = CartItem.create({
     name: "Google Pixel 3",
-    price: 2.83
+    price: 499
   });
-  expect(item.name).toBe("Google Pixel 3");
-  expect(item.price).toBe(2.83);
-  expect(item.tags).toStrictEqual([]);
+  const patches: any = [];
 
-  item.changeName("Pixel 3");
-  expect(item.name).toBe("Pixel 3");
-  item.changePrice(3.83);
-  expect(item.price).toBe(3.83);
+  onPatch(item, snapshot => {
+    patches.push(snapshot);
+  });
+
+  item.changeName("Google Pixel 4");
+  item.changePrice(799);
+  expect(patches).toMatchSnapshot();
 });
 
-it("should create a cart", () => {
+it("should create a cart instance", () => {
   const cart = Cart.create({
     items: [
       {
@@ -29,13 +31,74 @@ it("should create a cart", () => {
   expect(getSnapshot(cart)).toMatchSnapshot();
 });
 
-it("can add a new item to the cart", () => {
+it("can add new items to the cart", () => {
   const cart = Cart.create();
-  expect(cart.items.length).toBe(0);
+  const states: any = [];
+
+  onSnapshot(cart, snapshot => {
+    states.push(snapshot);
+  });
 
   cart.addCartItem({
     name: "New Item",
     price: 1
   });
-  expect(getSnapshot(cart)).toMatchSnapshot();
+  cart.addCartItem({
+    name: "New Item 2",
+    price: 2
+  });
+  expect(states).toMatchSnapshot();
+});
+
+it("can rename an item in the cart", () => {
+  const cart = Cart.create();
+  const patches: any = [];
+
+  onPatch(cart, snapshot => {
+    patches.push(snapshot);
+  });
+
+  cart.addCartItem({
+    name: "New Item",
+    price: 1
+  });
+
+  cart.items[0].changeName("New Item (new)");
+  expect(patches).toMatchSnapshot();
+});
+
+it("can return the total items of the cart", () => {
+  const cart = Cart.create();
+
+  cart.addCartItem({
+    name: "Item 1",
+    price: 1
+  });
+
+  expect(cart.totalItems).toBe(1);
+});
+
+it("can calculate the total price of the cart", () => {
+  const cart = Cart.create();
+
+  cart.addCartItem({
+    name: "Item 1",
+    price: 1
+  });
+  cart.addCartItem({
+    name: "Item 2",
+    price: 1
+  });
+
+  expect(cart.totalPrice).toBe(2);
+  let changed = 0;
+  reaction(() => cart.totalPrice, () => changed++);
+
+  expect(changed).toBe(0);
+  cart.items[0].changeName("Item 1 (new)");
+  expect(changed).toBe(0);
+
+  cart.items[0].changePrice(2);
+  expect(changed).toBe(1);
+  expect(cart.totalPrice).toBe(3);
 });
